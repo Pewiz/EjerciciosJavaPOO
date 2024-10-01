24c25dffa4e53d17a1506d23ef4f6d1c148ad050
Pewiz <dvilla20152025@gmail.com>
2024-10-01 17:21:16 -0300

    actualizando carpetas

--- ejercicio1/src/ClasesYObjetos/Cuadrilatero.java
+++ ejercicio1/src/ClasesYObjetos/Cuadrilatero.java
@@ -8,18 +8,27 @@ public class Cuadrilatero {
         this.lado1 = lado1;
         this.lado2 = lado2;
     }
-    public Cuadrilatero(int lado1){
+    public Cuadrilatero(int lado1) {
+        this.lado1 = this.lado2 = lado1;
+    }
+
+    public int getLado1(){
+        return lado1;
+    }
+    public int getLado2(){
+        return lado2;
+    }
+    public void setLado1(int lado1){
         this.lado1 = lado1;
-        this.lado2 = lado1;
     }
 
     public int obtenerArea(){
-        int area = lado1 * lado2;
-        return area;
-    }
-    public int obtenerPerimetro(){
-        int perimetro = 2*(lado1+lado2);
-        return perimetro;
+        int resultado = lado1 * lado2;
+        return resultado;
     }
 
+    public int obtenerPerimetro(){
+        int resultado = 2*(lado1+lado2);
+        return resultado;
+    }
 }
